49e36e507edae3dafb465011eca514bcf7c2da71
daniel-krgr <daniel-krugerdeal@hotmail.com>
2024-04-20 15:56:03 -0300

    update

--- TrabEstrutEClassDeDados/src/main/java/br/unipar/trabestruteclassdedados/TrabEstrutEClassDeDados.java
+++ TrabEstrutEClassDeDados/src/main/java/br/unipar/trabestruteclassdedados/TrabEstrutEClassDeDados.java
@@ -42,6 +42,37 @@ public class TrabEstrutEClassDeDados {
         while(!opcaoValida){
             
             String opcaoVetor = JOptionPane.showInputDialog(null, 
+                    "Deseja ordenar de qual forma?"
+                    + "\n1 - Crescente"
+                    + "\n2 - Decrescente",
+                    "Ordenação de Vetores",
+                    -1);
+            int opcao = Integer.parseInt(opcaoVetor);
+            
+            //"Menu" de opções de Ordenação que podem ser utilizadas
+            switch (opcao) {
+                case 1:
+                    opcaoValida = true;
+                    opcaoCrescente(vetor);
+                    break;
+                case 2:
+                    opcaoValida = true;
+                    opcaoDecrescente(vetor);
+                    break;
+                default:
+                    JOptionPane.showMessageDialog(null, 
+                            "Opção Inválida. Tente novamente.", 
+                            "Opção Inválida", 
+                            0);
+                    break;
+            }
+        }
+    }
+    public static void opcaoCrescente(int vetor[]){
+        boolean opcaoValida = false;
+        while(!opcaoValida){
+            
+            String opcaoVetor = JOptionPane.showInputDialog(null, 
                     "Selecione o método de ordenação que deseja utilizar:"
                     + "\n1 - Ordenação por Inserção"
                     + "\n2 - Ordenação por Seleção"
@@ -73,6 +104,44 @@ public class TrabEstrutEClassDeDados {
             }
         }
     }
+    public static void opcaoDecrescente(int vetor[]){
+        boolean opcaoValida = false;
+        while(!opcaoValida){
+            
+            String opcaoVetor = JOptionPane.showInputDialog(null, 
+                    "Selecione o método de ordenação que deseja utilizar:"
+                    + "\n1 - Ordenação por Inserção"
+                    + "\n2 - Ordenação por Seleção"
+                    + "\n3 - Ordenação Bolha", 
+                    "Ordenação de Vetores", 
+                    -1);
+            int opcao = Integer.parseInt(opcaoVetor);
+            
+            //"Menu" de opções de Ordenação que podem ser utilizadas
+            switch (opcao) {
+                case 1:
+                    opcaoValida = true;
+                    OrdenacaoPorInsercaoDecrescente(vetor);
+                    break;
+                case 2:
+                    opcaoValida = true;
+                    OrdenacaoPorSelecaoDrecrescente(vetor);
+                    break;
+                case 3:
+                    opcaoValida = true;
+                    OrdenacaoBolhaDrecrescente(vetor);
+                    break;
+                default:
+                    JOptionPane.showMessageDialog(null, 
+                            "Opção Inválida. Tente novamente.", 
+                            "Opção Inválida", 
+                            0);
+                    break;
+            }
+        }
+    }
+    
+    //Ordenações por Inserção, crescente e decrescente.=================================================================================
     public static void OrdenacaoPorInsercao(int vetor[]){
         long tempoInicio = System.currentTimeMillis();
         
@@ -110,7 +179,47 @@ public class TrabEstrutEClassDeDados {
                 "Ordenação por Inserção", 
                 -1);
     }
+    public static void OrdenacaoPorInsercaoDecrescente(int vetor[]){
+        long tempoInicio = System.currentTimeMillis();
+        
+        int [] vetorOrdenado = vetor.clone();
+        
+        int chave;
+        int j;
+        
+        for (int i = 1; i < vetorOrdenado.length; i++) {
+            chave = vetorOrdenado[i];
+            for (j = i-1; (j >= 0 && vetorOrdenado[j] < chave); j--) {
+                vetorOrdenado[j+1] = vetorOrdenado[j];
+            }
+            vetorOrdenado[j+1] = chave;
+        }
+        
+        String msg = "Vetor Original:      ";
+        for (int i = 0; i < vetor.length; i++) {
+            msg += vetor[i];
+            if(i < vetor.length-1)
+                msg += ", ";
+        }
+        msg += "\nVetor Ordenado:  ";
+        for (int i = 0; i < vetorOrdenado.length; i++) {
+            msg += vetorOrdenado[i];
+            if(i < vetorOrdenado.length-1)
+                msg += ", ";
+        }
+        long tempoFim = System.currentTimeMillis();
+        long tempoExecucao = tempoFim - tempoInicio;
+        
+        
+        JOptionPane.showMessageDialog(null,
+                msg += "\nTempo de Execução: "+ tempoExecucao+ "ms", 
+                "Ordenação por Inserção", 
+                -1);
+    }
    
+    
+    
+    //Ordenações por Seleção, crescente e drecescente.==================================================================================
     public static void OrdenacaoPorSelecao(int vetor[]){
         long tempoInicio = System.currentTimeMillis();
         
@@ -151,7 +260,50 @@ public class TrabEstrutEClassDeDados {
                 "Ordenação por Seleção", 
                 -1);
     }
+    public static void OrdenacaoPorSelecaoDrecrescente(int vetor[]){
+        long tempoInicio = System.currentTimeMillis();
+        
+        int [] vetorOrdenado = vetor.clone();
+        for (int i = 0; i < vetorOrdenado.length; i++) {
+            
+            int posicaoMaior = i;
+            for (int j = i+1; j < vetorOrdenado.length; j++) {
+                
+                if(vetorOrdenado[j] > vetorOrdenado[posicaoMaior]){
+                    posicaoMaior = j;
+                }               
+            }
+            
+            if(posicaoMaior != i){
+                int aux = vetorOrdenado[i];
+                vetorOrdenado[i] = vetorOrdenado[posicaoMaior];
+                vetorOrdenado[posicaoMaior] = aux;
+            }
+        }
+        String msg = "Vetor Original:      ";
+        for (int i = 0; i < vetor.length; i++) {
+            msg += vetor[i];
+            if(i < vetor.length-1)
+                msg += ", ";
+        }
+        msg += "\nVetor Ordenado:  ";
+        for (int i = 0; i < vetorOrdenado.length; i++) {
+            msg += vetorOrdenado[i];
+            if(i < vetorOrdenado.length-1)
+                msg += ", ";
+        }
+        long tempoFim = System.currentTimeMillis();
+        long tempoExecucao = tempoFim - tempoInicio;
+        
+        JOptionPane.showMessageDialog(null,
+                msg += "\nTempo de Execução: "+ tempoExecucao+ "ms", 
+                "Ordenação por Seleção", 
+                -1);
+    }
     
+    
+    
+    //Ordenações Bolha, crescente e decrescente.========================================================================================
     public static void OrdenacaoBolha(int vetor[]){
         long tempoInicio = System.currentTimeMillis();
         
@@ -193,4 +345,45 @@ public class TrabEstrutEClassDeDados {
                 "Ordenação Bolha", 
                 -1);
     } 
+    public static void OrdenacaoBolhaDrecrescente(int vetor[]){
+        long tempoInicio = System.currentTimeMillis();
+        
+        int [] vetorOrdenado = vetor.clone();
+        
+        boolean houveTroca= true;
+        
+        while(houveTroca){
+            houveTroca = false;
+            
+            for (int i = 0; i < vetorOrdenado.length -1; i++) {
+                
+                if(vetorOrdenado[i] < vetorOrdenado[i+1]){
+                    int aux = vetorOrdenado[i];
+                    vetorOrdenado[i] = vetorOrdenado[i+1];
+                    vetorOrdenado[i+1] = aux;
+                    houveTroca = true;
+                }
+            }
+        }
+        
+        String msg = "Vetor Original:      ";
+        for (int i = 0; i < vetor.length; i++) {
+            msg += vetor[i];
+            if(i < vetor.length-1)
+                msg += ", ";
+        }
+        msg += "\nVetor Ordenado:  ";
+        for (int i = 0; i < vetorOrdenado.length; i++) {
+            msg += vetorOrdenado[i];
+            if(i < vetorOrdenado.length-1)
+                msg += ", ";
+        }
+        long tempoFim = System.currentTimeMillis();
+        long tempoExecucao = tempoFim - tempoInicio;
+        
+        JOptionPane.showMessageDialog(null,
+                msg += "\nTempo de Execução: "+ tempoExecucao+ "ms", 
+                "Ordenação Bolha", 
+                -1);
+    }
 }
